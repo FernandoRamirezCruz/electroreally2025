de7a924f375a73a758aae805356f4c855ed367cb
FernandoRamirezCruz <ferrc200@gmail.com>
2025-05-06 11:58:59 -0600

    Graphics line
    This graphic is only static with default dates.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,73 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
+/* App.vue */
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  //import * as CanvasJS from '@canvasjs/charts';
+  export default {
+    data() {
+      return {
+        options: {
+          theme: "light2",
+          exportEnabled: true,
+          title: {
+            text: "Medicion del Espectometro UV VIS expectrofotometro"
+          },
+          axisY: {
+            title: "Fertitlity Rate"
+          },
+          toolTip: {
+            shared: true
+          },
+          legend: {
+            cursor: "pointer",
+            itemclick: function (e) {
+                if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                    e.dataSeries.visible = false;
+                } else {
+                    e.dataSeries.visible = true;
+                }
+                e.chart.render();
+            }
+          },
+          data: [{
+            type: "line",
+            name: "Germany",
+            showInLegend: true,
+            color: "#F7C705",
+            toolTipContent: "<img src=\"https://canvasjs.com/wp-content/uploads/images/gallery/javascript-column-bar-charts/germany.png\" style=\"height:11px;width:18px;\"> {name}: {y}",
+            dataPoints: [
+              { label: "2014", y: 1.47 },
+              { label: "2015", y: 1.5 },
+              { label: "2016", y: 1.6 },
+              { label: "2017", y: 1.57 },
+              { label: "2018", y: 1.57 },
+              { label: "2019", y: 1.54 }
+            ]
+          }, {
+            type: "line",
+            name: "United Kingdom",
+            showInLegend: true,
+            color: "#012066",
+            toolTipContent: "<img src=\"https://canvasjs.com/wp-content/uploads/images/gallery/javascript-column-bar-charts/uk.png\" style=\"height:11px;width:18px;\"> {name}: {y}",
+            dataPoints: [
+              { label: "2014", y: 1.81 },
+              { label: "2015", y: 1.80 },
+              { label: "2016", y: 1.79 },
+              { label: "2017", y: 1.74 },
+              { label: "2018", y: 1.68 },
+              { label: "2019", y: 1.65 }
+            ]
+          }]
+        },
+        styleOptions: {
+          width: "100%",
+          height: "360px"
+        }
+      }
+    }
   }
-}
 </script>
-
+<template>
+  <CanvasJSChart :options="options" :styles="styleOptions"/>
+</template>                              
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
